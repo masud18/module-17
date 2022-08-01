2f58c002183afe7f643f932d9c691e1e48a0945c
masud18 <ranamasud1838@gmail.com>
2022-08-01 09:59:39 +0600

    module17 ar problemes one and two

---
+++ module-17/problem1.js
@@ -0,0 +1,16 @@
+var x =56;
+if(x >= 50 && 60>x){
+    console.log("Grade D");
+}
+else if(x >= 60 && 70>x){
+    console.log("Grade C");
+}
+else if( x >= 70 && 80>x){
+    console.log("Grade B");
+}
+else if( x >= 80 && 90>x){
+    console.log("Grade A");
+}
+else{
+    console.log("Ha Ha fail")
+}
---
+++ module-17/problem2.js
@@ -0,0 +1,14 @@
+var signal = ["red", "yellow", "green"]
+var colors ;
+if(signal[0] === colors){
+    console.log("Danger")
+}
+else if(signal[1]=== colors){
+    console.log("Stop")
+}
+else if(signal[2]=== colors){
+console.log("Let's go")
+}
+else{
+    console.log("No trafick rules")
+}
