0442bb7982186cd77a387ed9e10debaedff0d955
masud18 <ranamasud1838@gmail.com>
2022-08-03 21:40:22 +0600

    module-18.5 practis

---
+++ module-18.5/practis.js
@@ -0,0 +1,202 @@
+// 3,4,5 questin answer
+// var variableDeclaration = 20;
+// variableDeclaration = 40//Variable Cahange
+// console.log(variableDeclaration)
+
+//6. Different type of variable 
+// var x = 'String'; //String type
+// var y = 40;//Number type
+// var z = true //Boolean type
+//7.
+//Primitive Data
+// number ; string ; boolean; null ; undefiend;
+//none-primitive Data
+//object ; array
+//8 variable declaration nameing syste
+// var fristName = "Masud";
+// var person_Name = "rana";
+// var price1 = 20;
+//no name of variable declaration
+// var 10x = 20;
+//rejap keyword not writing
+// 9. =;-;+;/;%
+// var x =(20-10)+(10/5)*(3*3)%10;
+// console.log(x)
+//10 Short heand 
+// var x =20;
+//  x++;
+//  x=x+1;
+//  x+=1;
+// var y = 30;
+//  y--;
+//  y=y-1;
+//  y-=1;
+// var z = 40;
+//  z*=4;
+//  z=z*4;
+// var xx = 50;
+// xx/=10;
+// xx =xx/10;
+// 11. ++; --
+//++ it is value incriment 1 1 1 1 ;
+// -- it is value decriment 1 1 1 1;
+//12.parseInt; parseFloat; toFixed;
+//parseInt;Work:It is works puno shonkha nibe;
+// var num1 = 20.10;
+// var numParseInt =parseInt(num1);
+// console.log(numParseInt)
+//ParseFloat Works It is works dhosomick shonkha nibe;
+// var num1 = 20.10;
+// var numparseFloat =parseFloat(num1);
+// console.log(numparseFloat);
+// var num2 = 20.467;
+// var yy = num2.toFixed(1)
+// console.log(yy)
+//13. Array is a object 
+// var arr =["apple", "orange", "mango"];
+//14. Array ar man koyta ace //Answer 3;
+// var arr =["apple", "orange", "mango"];
+// var arrLangth = arr.length;
+// console.log(arrLangth);
+// Answer 3;
+//15. Arrary element frist index of 0;1;2;3 avabe jay|
+// var arr = [5, 4, 3, 2];
+// arr[2] = 20;//Man Change kira hoice
+// console.log(arr)
+//16. kono array index ar man -1 oi array modhe oi elementi nai tai -1 dhakhay
+//১৬. কোন একটা উপাদানের index এর মান -১ বলতে কি বুঝায় 
+// var arr = [2, 33, 5, 30];
+// var arr2 = arr.indexOf(34);
+// console.log(arr2)
+//১৭. কিভাবে index দিয়ে কোন একটা array এর মধ্যে উপাদান এর মান খুঁজে বের করতে পারো 
+// var arr = [2, 33, 5, 30];
+// var arr2 = arr[1];
+// console.log(arr2)
+// ১৮. কিভাবে কোন একটা index পজিশন এ array এর উপাদান এর মান চেইঞ্জ করতে পারবে 
+// var arr = [2, 33, 5, 30];
+// arr[2] = 10;
+// console.log(arr)
+//১৯. একটা Array এর মধ্যে কোন একটা উপাদান এর মান তোমাকে দেয়া আছে এখন সেটার index তুমি কিভাবে খুঁজে বের করতে পারো?
+// var arr = [2, 33, 5, 30];
+// var arr2 = arr.indexOf(30);
+// console.log(arr2)
+// ২০. ধরো, কোন একটা ইনডেক্স দিয়ে উপাদান খুঁজতে গেছো। কিন্তু সেটার মান না দিয়ে তোমাকে undefined দেখাচ্ছে। সেটা দেখে তুমি কি বুঝবে? (একটু গুগলে সার্চ দাও। আমরা কোর্স এ এইটা আলোচনা করিনি। তারপরেও চেষ্টা করে দেখো)
+// kono array index ar man -1 oi array modhe oi elementi nai tai -1 dhakhay
+// var arr = [2, 33, 5, 30];
+// var arr2 = arr.indexOf(34);
+// console.log(arr2)
+//২১. কোন একটা Array এর মধ্যে লাস্ট উপাদান হিসেবে কোন উপাদান হিসেবে যোগ করতে চাইলে কিভাৱে যোগ করবে। আবার Array থেকে শেষের উপাদান টা বের করে দিতে চাইলে কিভাবে বের করে দিবে
+// var arr = [2, 33, 5, 30];
+// arr.push(500000);
+// console.log(arr)
+//pop
+// var arr = [2, 33, 5, 30];
+// arr.pop();
+// console.log(arr)
+//২২. কোন একটা Array এর মধ্যে প্রথম উপাদান হিসেবে কোন উপাদান হিসেবে যোগ করতে চাইলে কিভাৱে যোগ করবে। আবার Array থেকে প্রথম উপাদান টা বের করে দিতে চাইলে কিভাবে বের করে দিবে
+// unshift madhome element dhukhano
+/* var arr = [2, 33, 5, 30];
+arr.unshift(400);
+console.log(arr) */
+// shift ar madhome element dhukhano
+/* var arr = [2, 33, 5, 30];
+arr.shift();
+console.log(arr) */
+// ২৩. তুলনা কিভাবে করতে হয়। এইগুলার মানে কি: >, <, ==, !=, <=, >=, ===, !==, &&, ।। 
+// 1.  > geterthan boro 5>3 3 ar thake three boro 5
+// 1.  < lessthan choto 5<3 3 ar thake three boro 5
+// == equeal man golo soman 5==5;
+//!= not equal man golo soman na 5!=5
+//=== a ti two kaj kora equal dhake and type dhake //duiti ak hothe hobe
+// !==a ti two kaj kora not equal dhake and type dhake //duiti ak hothe hobe
+//&& ai operator ta dhui dhiker sotho thik hoy thahole kaj hoy;
+//|| ai operator ta dhui dhiker sothotr modhe akta thik hoy thahole kaj hoy;
+//২৪. তোমার কাছে: ৮০০০০ টাকার বেশি হলে তুমি mac কিনবে, ৬০ টাকার বেশি হলে gaming ল্যাপটপ কিনবে, ৪০ হাজার টাকার বেশি হলে lenovo yoga কম্পিউটার কিনবে , ২০ হাজার টাকার বেশি হলে পুরান ল্যাপটপ কিনবে। না হয় তুমি মোবাইল দিয়ে কাজ চালাবে। 
+/* var myMany =2000;
+if(myMany>80000){
+    console.log('I am buying mac')
+}
+else if(myMany<80000 && 60000<myMany){
+    console.log('I am buying gaming leptop')
+}
+else if(myMany>20000 &&60000>myMany){
+    console.log('I am buying lenovo yoga leptop')
+}
+else{
+    console.log('I am buy mobile phoen')
+} */
+// ২৫. আসকে আমার মন ভালো নেই -- এই কথা ৩৯ বার আউটপুট হিসেবে দেখাও। 
+// for (var i = 0; i<40;i++){
+//     console.log(i +"Aske Amar mon valo nai")
+// }
+//২৬. while লুপ কিভাবে কাজ করে jodi loop koy bar ghurbe na dhaya thaka thahole ati bebohar hobe
+/* var x = 2;
+while(x<=10){
+    console.log(x);
+    x++;
+} */
+// ২৭. for লুপ কিভাবে কাজ করে jodi loop koy bar ghurbe  dhaya thaka thahole ati bebohar hobe
+// var i=0;
+// for(i;i<10;i++){
+//     console.log(i)
+// }
+// ২৮. while লুপ এর মধ্যে লুপ ভেরিয়েবল চেইঞ্জ না করলে কি সমস্যা হয়। 
+//Answer:while loop ar modhe loop variable change na korle while loop cholthe thakbe
+/* var  i = 0;
+while(i<10){
+    console.log(i);
+    //ai khane loop variable change kora hoy nai tai cholthe thakbe
+} */
+//২৯. একটা কোড লিখে ৫৮ থেকে ৯৮ পর্যন্ত যত সংখ্যা আছে সেগুলাকে দেখাও 
+/* var number = 58;
+while(number<=98){
+    console.log(number)
+    number++;
+} */
+//৩০.একটা কোড লিখে ৪১২ থেকে ৪৫৬ পর্যন্ত যত জোর সংখ্যা আছে সেগুলাকে দেখাও  
+/* var number = 412;
+while(number<=456){
+    console.log(number);
+    number+=2;
+} */
+// ৩১. একটা কোড লিখে ৫৮১ থেকে ৬২৩ পর্যন্ত যত বিজোড় সংখ্যা আছে সেগুলাকে দেখাও 
+// for(var i =581; i<=623; i=i+2){
+//     console.log(i)
+// }
+// ৩২.while আর for loop এর মধ্যে পার্থক্য কি 
+//while loop bola thakana koy bar cholbe
+//for loop bola thaka koy bar cholbe
+//৩৩ তুমি এতদিন যা যা জিনিস শিখছো সেগুলার নাম দিয়ে একটা array বানাও। তারপর একটা for লুপ দিয়ে সেই array এর সব উপাদান কে আউটপুট হিসেবে দেখাও। 
+/* var arr = ['number', 'string', 'boolean', ' undefied', 'null', 'array', 'object', 'push', 'pop', 'shift', 'unshift','toFixed', 'parseInt', 'parseFload', 'loop', 'while', 'if', 'else', 'condition'];
+for(var i=0; i<arr.length; i++){
+    console.log(arr[i]);
+} */
+//৩৪. তুমি এতদিন পর্যন্ত যে যে মডেলের মোবাইল ফোন ইউজ করেছো সেগুলার নাম দিয়ে একটা array বানাও। তারপর একটা while লুপ দিয়ে সেই array এর উপাদান গুলা একটা একটা করে আউটপুট হিসেবে দেখাও 
+var arr = ['itel', 'opp', 'samsung', 'redmi', 'i-phone'];
+var i=0;
+while(i<arr.length){
+    console.log(arr[i]);
+    i++;
+}
+//৩৫. একটা ফর লুপ চালাও। ৩০ থেকে ৮৬ পর্যন্ত। আর এই লুপ ৪৪ এ গেলে ব্রেক করবে। সেই জিনিস কোড করে দেখাও 
+// var i = 30;
+// while(i<86){
+//     if(i==44){
+//         break;
+//     }
+//     i++;
+//     console.log(i)
+// }
+//৩৬. তোমার যত বই আছে সেগুলার দাম নিয়ে একটা array লিখে ফেলো। যে বই গুলোর দাম ২০০ টাকার উপরে সেগুলাকে স্কিপ করবে। অর্থাৎ সেগুলাকে আউটপুট হিসেবে দেখাবে না। বাকিদের কে আউটপুট হিসেবে দেখাবে। দেখো করতে পারো কিনা। 
+var arr = [100, 230, 500, 66, 70, 80];
+for(var i = 0;i<arr.length; i++){
+    if(arr[i]>200){
+        continue;
+    }
+    console.log(arr[i])
+}
+
+
+
+
+
